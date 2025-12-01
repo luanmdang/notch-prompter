03bd14c21dac22eb3de77becf0cdd7d2125b7934
Luan Dang <luandang2023@gmail.com>
2025-11-30 20:46:22 -0800

    Initial Commit

---
+++ Notch Prompter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notch Prompter
+//
+//  Created by Luan Dang on 11/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notch Prompter/Notch_PrompterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notch_PrompterApp.swift
+//  Notch Prompter
+//
+//  Created by Luan Dang on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Notch_PrompterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
